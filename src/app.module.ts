8cffd05bc615ad37b3af6ef064705ace9affb707
Vero <dipmveronica@gmail.com>
2024-06-04 09:40:48 -0300

    adding users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ActivitiesModule } from './activities/activities.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './users.module';
 
 
 @Module({
@@ -15,6 +17,7 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: `mongodb+srv://${configService.get('MONGODB_USERNAME')}:${configService.get('MONGODB_PASSWORD')}@cluster0.9ambpnq.mongodb.net/?retryWrites=true&w=majority&appName=${configService.get('MONGODB_DATABASE_NAME')}`,
       }),
       inject: [ConfigService],
-    })],
+    }),
+    UsersModule],
 })
 export class AppModule { }
